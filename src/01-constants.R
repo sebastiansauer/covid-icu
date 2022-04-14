c4c03f6ca6f769d02e077f9ad416ebe235585ca9
Sebastian Sauer <ssauer@posteo.de>
2022-04-14 18:03:47 +0200

    update paths

--- src/01-constants.R
+++ src/01-constants.R
@@ -1,15 +1,56 @@
 
-data_raw_path <- "/Users/sebastiansaueruser/Google Drive/research/Covid19/covid-icu-old/data/raw/CovidDA_CA_randomisierte Version_bearbeitet230322.xlsx"
-
-data_raw_small_path <- "data/raw/d-raw-small.csv"
+# Overall flags -----------------------------------------------------------
 
 
-data_processed_path <- "/Users/sebastiansaueruser/Google Drive/research/Covid19/covid-icu-old/data/processed/"
 
-
+#dataset <- "small"
+dataset <- "real"
 
 rerun_all <- FALSE
-write_to_disk <- FALSE
+write_to_disk <- TRUE
+
+
+
+
+# Data Paths -------------------------------------------------------------------
+
+
+path_prefix_real <- "/Users/sebastiansaueruser/Google Drive/research/Covid19/covid-icu-old/"
+path_prefix_small <- "./"
+
+if (dataset == "small") path_prefix <- path_prefix_small
+if (dataset == "real") path_prefix <- path_prefix_real
+
+data_raw_path <- paste0(path_prefix, "data/raw/CovidDA_CA_randomisierte Version_bearbeitet230322.xlsx")
+
+data_processed_path <- paste0(path_prefix, "data/processed/data-prepared.csv")
+
+
+# Output Paths -------------------------------------------------------------------
+
+
+
+rmdfile01 <- "01-preparing-the-data"
+rmdfile02 <- "02-descriptive-analysis"
+rmdfile03 <- "03-vis"
+rmdfile04 <- "04-modelling"
+
+
+# params <- list(dataset = "small")
+# params <- list(dataset = NULL)
+
+
+
+xgb01_outputfile <- paste0(path_prefix, "models/xgb01.rds")
+logistic01_outputfile <- paste0(path_prefix, "models/logistic01.rds")
+rf01_outputfile <- paste0(path_prefix, "models/rf01.rds")
+final_rf_fit_file <-  paste0(path_prefix, "models/rf_final_01.rds")
+
+
+figs_prefix_path <- paste0(path_prefix, "figs/")
+
+
+# Particular variables ----------------------------------------------------
 
 
 forbidden_vars <-c(
@@ -26,6 +67,3 @@ forbidden_vars <-c(
 
 
 
-rmdfile01 <- "01-preparing-the-data"
-rmdfile02 <- "02-descriptive-analysis"
-rmdfile03 <- "03-vis"
